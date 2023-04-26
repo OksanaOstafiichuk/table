25c5988f28a91be472196210e2269efd4d338ff1
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2023-04-26 19:32:56 +0300

    add sort table

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { NewUserForm } from './NewUserForm/NewUserForm';
+import { Table } from './Table/Table';
+
 export const App = () => {
+  const [dataTable, setDataTable] = useState([]);
+  const [btnSort, setBtnSort] = useState(false);
+
+  const addUser = (name, email, age) => {
+    const user = { id: uuidv4(), name, email, age };
+    setDataTable(prevState => [...prevState, user]);
+  };
+
+  const handelSortByName = () => {
+    if (!btnSort) {
+      const dataBySort = [...dataTable].sort((a, b) =>
+        a.name.localeCompare(b.name)
+      );
+      setDataTable(dataBySort);
+      setBtnSort(!btnSort);
+    } else {
+      const dataBySort = [...dataTable].sort((a, b) =>
+        b.name.localeCompare(a.name)
+      );
+      setDataTable(dataBySort);
+      setBtnSort(!btnSort);
+    }
+  };
+
+  const handelSortByAge = () => {
+    if (!btnSort) {
+      const dataBySort = [...dataTable].sort((a, b) => a.age - b.age);
+      setDataTable(dataBySort);
+      setBtnSort(!btnSort);
+    } else {
+      const dataBySort = [...dataTable].sort((a, b) => b.age - a.age);
+      setDataTable(dataBySort);
+      setBtnSort(!btnSort);
+    }
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
+    <div>
       Create table
+      <NewUserForm onSubmit={addUser} />
+      <button type="button" onClick={handelSortByName}>
+        Sort by name
+      </button>
+      <button type="button" onClick={handelSortByAge}>
+        Sort by age
+      </button>
+      <Table data={dataTable} />
     </div>
   );
 };
